2a1401ce2f4ed67b3389276f3e0193b44bc374f6
Dane Filipczak <dane@imaginationrock.com>
2018-01-15 14:44:24 -0500

    basic three.js integration

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,33 @@
 var sevenths = [];
-window.onload = function(){
+var scene;
+var camera;
+
+window.onload = function() {
+
+
+	scene = new THREE.Scene();
+	//scene.add(group);
+	scene.background = new THREE.Color('olive');
+	camera = new THREE.PerspectiveCamera(75, window.innerWidth / (window.innerHeight), 0.1, 2500);
+	camera.position.z = 30;
+	// camera.position.y = 20;
+
+	// camera.far=10
+
+	
+
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.body.appendChild(renderer.domElement);
+	renderer.domElement.style = "position:fixed; top:0px; left:0px"
+
+	orbit = new THREE.OrbitControls(camera, renderer.domElement)
+
+	ambient = new THREE.HemisphereLight(0x404040); // soft white light
+	ambient.position.y = -10
+	ambient.position.z = 5
+	scene.add(ambient);
+
 	for(var i = 0; i<12; i++){
 		var m7 = new Seventh(i, 'm7')
 		var dom = new Seventh(i, 'dom')
@@ -14,4 +42,25 @@ window.onload = function(){
 			}
 		}
 	}
+
+	
+	// var materialArray = [];
+	// for (var i = 0; i < 6; i++)
+	// 	materialArray.push(new THREE.MeshBasicMaterial({
+	// 		map: loader.load(imagePrefix + directions[i] + imageSuffix),
+	// 		side: THREE.BackSide
+	// 	}));
+	// var skyMaterial = new THREE.MultiMaterial(materialArray);
+	// // var skyMaterial = new THREE.MeshPhongMaterial( {color:'black'} );
+	// // skyMaterial.side = THREE.BackSide;
+	// skyBox = new THREE.Mesh(skyGeometry, skyMaterial);
+	// skyBox.position.setComponent(1, 1000)
+	// skyBox.rotation.y=15
+	// scene.add(skyBox)
+
+
+
+	//
+
+	render();
 }
--- js/seventh.js
+++ js/seventh.js
@@ -1,45 +1,67 @@
-function Seventh(root_, type_){
+function Seventh(root_, type_) {
 	this.root = root_;
 	this.type = type_;
 	this.chroma = []
-	this.linkedTo=[];
+	this.linkedTo = [];
 	this.initialize()
+	this.geometry = new THREE.SphereGeometry(this.r, 32, 32);
+	// this.material = new THREE.MeshPhongMaterial( {color: 'yellow'} );
+
+
+	// var shader = THREE.FresnelShader;
+	// var uniforms = THREE.UniformsUtils.clone(shader.uniforms);
+	// uniforms["tCube"].value = textureCube;
+	// this.material = new THREE.ShaderMaterial({
+	// 	uniforms: uniforms,
+	// 	vertexShader: shader.vertexShader,
+	// 	fragmentShader: shader.fragmentShader
+	// });
+	this.material = new THREE.MeshPhongMaterial({
+        color: 'cyan'
+    });
+
+	this.sphere = new THREE.Mesh(this.geometry, this.material);
+	scene.add(this.sphere);
+	var splay=100;
+	this.sphere.position.x+=(Math.random()*splay)-splay/2;
+	this.sphere.position.y+=(Math.random()*splay)-splay/2;
+	this.sphere.position.z+=(Math.random()*splay)-splay/2;
 }
 
-Seventh.prototype.initialize = function(){
+Seventh.prototype.initialize = function() {
 	this.chroma.push( //everyone gets a root and a minor seventh
 		this.root,
-		(this.root+10)%12
-		)
-	switch(this.type){
+		(this.root + 10) % 12
+	)
+	switch (this.type) {
 		case 'm7':
 			this.chroma.push(
-				(this.root+3)%12,
-				(this.root+7)%12
-				);
+				(this.root + 3) % 12,
+				(this.root + 7) % 12
+			);
 			break;
 		case 'hd':
 			this.chroma.push(
-				(this.root+3)%12,
-				(this.root+6)%12
-				);
+				(this.root + 3) % 12,
+				(this.root + 6) % 12
+			);
 			break;
 		case 'fr':
 			this.chroma.push(
-				(this.root+4)%12,
-				(this.root+6)%12
-				);
+				(this.root + 4) % 12,
+				(this.root + 6) % 12
+			);
 			break;
 		case 'dom':
 			this.chroma.push(
-				(this.root+4)%12,
-				(this.root+7)%12
-				);
+				(this.root + 4) % 12,
+				(this.root + 7) % 12
+			);
 			break;
 	}
 }
 
-Seventh.prototype.checkParsimony = function(otherSeventh){ 
+Seventh.prototype.checkParsimony = function(otherSeventh) {
 	//how many chroma do we have in common?
 	var intersection = otherSeventh.chroma.filter((c) => this.chroma.includes(c))
 	return intersection.length
